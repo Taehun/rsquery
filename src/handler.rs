8e70f200d83f1eb6db9a7f0217c5b74d1863ddda
Taehun Kim <kth3321@gmail.com>
2023-07-20 01:09:47 +0900

    Hello world

---
+++ src/handler.rs
@@ -0,0 +1,183 @@
+use crate::{
+    model::{AppState, QueryOptions, Todo, UpdateTodoSchema},
+    response::{GenericResponse, SingleTodoResponse, TodoData, TodoListResponse},
+};
+use actix_web::{delete, get, patch, post, web, HttpResponse, Responder};
+use chrono::prelude::*;
+use uuid::Uuid;
+
+#[get("/health")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "RSQuery REST API server is running!";
+
+    let response_json = &GenericResponse {
+        status: "success".to_string(),
+        message: MESSAGE.to_string(),
+    };
+    HttpResponse::Ok().json(response_json)
+}
+
+#[get("/todos")]
+pub async fn todos_list_handler(
+    opts: web::Query<QueryOptions>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let todos = data.todo_db.lock().unwrap();
+
+    let limit = opts.limit.unwrap_or(10);
+    let offset = (opts.page.unwrap_or(1) - 1) * limit;
+
+    let todos: Vec<Todo> = todos.clone().into_iter().skip(offset).take(limit).collect();
+
+    let json_response = TodoListResponse {
+        status: "success".to_string(),
+        results: todos.len(),
+        todos,
+    };
+    HttpResponse::Ok().json(json_response)
+}
+
+#[post("/todos")]
+async fn create_todo_handler(
+    mut body: web::Json<Todo>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let mut vec = data.todo_db.lock().unwrap();
+
+    let todo = vec.iter().find(|todo| todo.title == body.title);
+
+    if todo.is_some() {
+        let error_response = GenericResponse {
+            status: "fail".to_string(),
+            message: format!("Todo with title: '{}' already exists", body.title),
+        };
+        return HttpResponse::Conflict().json(error_response);
+    }
+
+    let uuid_id = Uuid::new_v4();
+    let datetime = Utc::now();
+
+    body.id = Some(uuid_id.to_string());
+    body.completed = Some(false);
+    body.createdAt = Some(datetime);
+    body.updatedAt = Some(datetime);
+
+    let todo = body.to_owned();
+
+    vec.push(body.into_inner());
+
+    let json_response = SingleTodoResponse {
+        status: "success".to_string(),
+        data: TodoData { todo },
+    };
+
+    HttpResponse::Ok().json(json_response)
+}
+
+#[get("/todos/{id}")]
+async fn get_todo_handler(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
+    let vec = data.todo_db.lock().unwrap();
+
+    let id = path.into_inner();
+    let todo = vec.iter().find(|todo| todo.id == Some(id.to_owned()));
+
+    if todo.is_none() {
+        let error_response = GenericResponse {
+            status: "fail".to_string(),
+            message: format!("Todo with ID: {} not found", id),
+        };
+        return HttpResponse::NotFound().json(error_response);
+    }
+
+    let todo = todo.unwrap();
+    let json_response = SingleTodoResponse {
+        status: "success".to_string(),
+        data: TodoData { todo: todo.clone() },
+    };
+
+    HttpResponse::Ok().json(json_response)
+}
+
+#[patch("/todos/{id}")]
+async fn edit_todo_handler(
+    path: web::Path<String>,
+    body: web::Json<UpdateTodoSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let mut vec = data.todo_db.lock().unwrap();
+
+    let id = path.into_inner();
+    let todo = vec.iter_mut().find(|todo| todo.id == Some(id.to_owned()));
+
+    if todo.is_none() {
+        let error_response = GenericResponse {
+            status: "fail".to_string(),
+            message: format!("Todo with ID: {} not found", id),
+        };
+        return HttpResponse::NotFound().json(error_response);
+    }
+
+    let todo = todo.unwrap();
+    let datetime = Utc::now();
+    let title = body.title.to_owned().unwrap_or(todo.title.to_owned());
+    let content = body.content.to_owned().unwrap_or(todo.content.to_owned());
+    let payload = Todo {
+        id: todo.id.to_owned(),
+        title: if !title.is_empty() {
+            title
+        } else {
+            todo.title.to_owned()
+        },
+        content: if !content.is_empty() {
+            content
+        } else {
+            todo.content.to_owned()
+        },
+        completed: if body.completed.is_some() {
+            body.completed
+        } else {
+            todo.completed
+        },
+        createdAt: todo.createdAt,
+        updatedAt: Some(datetime),
+    };
+    *todo = payload;
+
+    let json_response = SingleTodoResponse {
+        status: "success".to_string(),
+        data: TodoData { todo: todo.clone() },
+    };
+
+    HttpResponse::Ok().json(json_response)
+}
+
+#[delete("/todos/{id}")]
+async fn delete_todo_handler(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
+    let mut vec = data.todo_db.lock().unwrap();
+
+    let id = path.into_inner();
+    let todo = vec.iter_mut().find(|todo| todo.id == Some(id.to_owned()));
+
+    if todo.is_none() {
+        let error_response = GenericResponse {
+            status: "fail".to_string(),
+            message: format!("Todo with ID: {} not found", id),
+        };
+        return HttpResponse::NotFound().json(error_response);
+    }
+
+    vec.retain(|todo| todo.id != Some(id.to_owned()));
+    HttpResponse::NoContent().finish()
+}
+
+pub fn config(conf: &mut web::ServiceConfig) {
+    let scope = web::scope("/api")
+        .service(health_checker_handler)
+        .service(todos_list_handler)
+        .service(create_todo_handler)
+        .service(get_todo_handler)
+        .service(edit_todo_handler)
+        .service(delete_todo_handler);
+
+    conf.service(scope);
+}
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+mod handler;
+mod model;
+mod response;
+
+use actix_cors::Cors;
+use actix_web::middleware::Logger;
+use actix_web::{http::header, web, App, HttpServer};
+use model::AppState;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::init();
+
+    let todo_db = AppState::init();
+    let app_data = web::Data::new(todo_db);
+
+    println!("🚀 Server started successfully");
+
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_origin("http://localhost:3000/")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
+        App::new()
+            .app_data(app_data.clone())
+            .configure(handler::config)
+            .wrap(cors)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
+}
